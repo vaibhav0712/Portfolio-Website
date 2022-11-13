e88c5b4c9cf5b2e443d24d8cb6e6c343c67fbf83
vaibhav <98164944+DEADPOOL712@users.noreply.github.com>
2022-11-14 00:01:05 +0530

    12:00 am

--- script.js
+++ script.js
@@ -3,6 +3,34 @@ const python = document.querySelector(".pythonT");
 const c = document.querySelector(".cT");
 const skillbar = document.querySelector(".skill-bar");
 
+const snack = document.querySelectorAll(".snack");
+for (let i = 0; i < snack.length; i++) {
+  snack[i].addEventListener("click", function () {
+    location.href = "https://github.com/DEADPOOL712/snake_python";
+  });
+}
+const todo = document.querySelectorAll(".todo");
+for (let i = 0; i < todo.length; i++) {
+  todo[i].addEventListener("click", function () {
+    location.href = "https://todobyvaibhav.netlify.app/";
+    console.log("todo");
+  });
+}
+const cal = document.querySelectorAll(".cal");
+for (let i = 0; i < cal.length; i++) {
+  cal[i].addEventListener("click", function () {
+    location.href = "https://calbyvaibhav.netlify.app/";
+    console.log("cal");
+  });
+}
+const pingpong = document.querySelectorAll(".pingpong");
+for (let i = 0; i < pingpong.length; i++) {
+  pingpong[i].addEventListener("click", function () {
+    location.href = "https://github.com/DEADPOOL712/python";
+    console.log("pingpong");
+  });
+}
+
 const lazyLoad = function (entries) {
   const [entry] = entries;
   console.log(entry);
